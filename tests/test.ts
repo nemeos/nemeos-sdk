cc468259f1fe7e6435d3edc9d63300c83de21059
rigwild <me@rigwild.dev>
2024-07-06 00:13:21 +0200

    Add tests

---
+++ tests/test.ts
@@ -0,0 +1,33 @@
+import * as ethers from 'ethers'
+import { NemeosSDK } from '../src/index.js'
+
+async function main() {
+  if (!process.env.WALLET_PRIVATE_KEY || !process.env.INFURA_ENDPOINT_WITH_API_KEY) {
+    const missingVariables = Object.entries({
+      WALLET_PRIVATE_KEY: process.env.WALLET_PRIVATE_KEY,
+      INFURA_ENDPOINT_WITH_API_KEY: process.env.INFURA_ENDPOINT_WITH_API_KEY,
+    })
+      .filter(([_, value]) => !value)
+      .map(([key]) => key)
+      .join(', ')
+    throw new Error(`Missing environment variables: ${missingVariables}.`)
+  }
+
+  const provider = new ethers.JsonRpcProvider(process.env.INFURA_ENDPOINT_WITH_API_KEY)
+  const wallet = new ethers.Wallet(process.env.WALLET_PRIVATE_KEY, provider)
+  const nemeosSdk = new NemeosSDK(wallet)
+
+  const nemeosPoolAddress = '0x812db15b8Bb43dBA89042eA8b919740C23aD48a3'
+  const cyberKongzAddress = '0x15cd1cfCd48C06cfC44D433D66C7a9fE06b2C2c3'
+  const nemeosPool = nemeosSdk.getPool({
+    nemeosPoolAddress,
+    nftCollectionAddress: cyberKongzAddress,
+  })
+
+  // await nemeosPool.startLoan(224, 90)
+  // await nemeosPool.registerCustomerEmailAddress('nemeos.hello.testing1234@yopmail.com')
+  await nemeosPool.retrieveLoan(224)
+  // await nemeosPool.payNextLoanStep(224)
+}
+
+main().catch(console.error)
