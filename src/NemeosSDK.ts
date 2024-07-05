8056a2e6d808f3b9baa176be24952e7e7d2cfe03
rigwild <me@rigwild.dev>
2024-07-05 23:23:04 +0200

    Add retrieveLoan

--- src/NemeosSDK.ts
+++ src/NemeosSDK.ts
@@ -1,6 +1,6 @@
-import ethers from 'ethers'
-import { assertValidHexAddress, NemeosSDKError } from './utils.js'
+import * as ethers from 'ethers'
 import { NemeosBackendClient } from './NemeosBackendClient.js'
+import { assertValidHexAddress, NemeosSDKError } from './utils.js'
 
 const NEMEOS_POOL_HUMAN_ABI = [
   'function buyNFT(uint256 tokenId, uint256 priceOfNFT, uint256 nftFloorPrice, uint256 priceOfNFTIncludingFees, address settlementManager, uint256 loanTimestamp, uint256 loanDurationInSeconds, bytes orderExtraData, bytes oracleSignature) payable',
@@ -50,13 +50,14 @@ class NemeosPool {
     )
   }
 
-  public async startLoan(nftId: number, loanDurationDays: number): Promise<ethers.ContractTransactionReceipt | null> {
+  public async startLoan(nftId: number, loanDurationDays: number): Promise<ethers.ContractTransactionReceipt> {
     const borrowerAddress = await this.signer.getAddress()
 
     console.log(
       `[nemeos-sdk][startLoan] Starting loan from borrowerAddress=${borrowerAddress} for ` +
         `nftCollectionAddress=${this.nftCollectionAddress}, nftId=${nftId}, loanDurationDays=${loanDurationDays}`,
     )
+
     const startLoanData = await this.nemeosBackendClient.fetchStartLoanData(
       borrowerAddress,
       this.nftCollectionAddress,
@@ -65,7 +66,7 @@ class NemeosPool {
     )
     const feeOverides = await this.getFeeOverrides()
 
-    console.log('[nemeos-sdk][startLoan] Call Pool.BuyNFT() with startLoanData=', startLoanData, ', feeOverides=', feeOverides)
+    console.log('[nemeos-sdk][startLoan] Call Pool.buyNFT() with startLoanData=', startLoanData, ', feeOverides=', feeOverides)
     const tx: ethers.ContractTransactionResponse = await this.poolContract.buyNFT(
       startLoanData.customerBuyNftParameters.tokenId,
       startLoanData.customerBuyNftParameters.priceOfNFT,
@@ -86,9 +87,44 @@ class NemeosPool {
     const receipt = await tx.wait()
     if (receipt?.status !== 1) {
       console.error('[nemeos-sdk][startLoan] Transaction failed! Tx receipt', receipt)
-      throw new NemeosSDKError('Pool.BuyNFT() Transaction failed!')
+      throw new NemeosSDKError('Pool.buyNFT() Transaction failed!')
     }
     console.log('[nemeos-sdk][startLoan] Transaction successful! Tx receipt', receipt)
+    return receipt
+  }
+
+  public async retrieveLoan(nftId: number): Promise<Loan> {
+    const borrowerAddress = await this.signer.getAddress()
+
+    console.log(`[nemeos-sdk][retrieveLoan] Call Pool.retrieveLoan() with nftId=${nftId}, borrowerAddress=${borrowerAddress}`)
+    const loan: Loan = loanProxyToObjectMapper(await this.poolContract.retrieveLoan(nftId, borrowerAddress))
+    console.log('[nemeos-sdk][retrieveLoan] Retrieved loan data from contract, loan=', loan)
+    return loan
+  }
+
+  public async payNextLoanStep(nftId: number): Promise<ethers.ContractTransactionReceipt> {
+    const borrowerAddress = await this.signer.getAddress()
+
+    console.log(
+      `[nemeos-sdk][payNextLoanStep] Paying next loan step from borrowerAddress=${borrowerAddress} for ` +
+        `nftCollectionAddress=${this.nftCollectionAddress}, nftId=${nftId}`,
+    )
+
+    const loan = await this.retrieveLoan(nftId)
+
+    console.log(
+      `[nemeos-sdk][payNextLoanStep] Call Pool.refundLoan() with nftId=${nftId}, borrowerAddress=${borrowerAddress}, loan.nextPaymentAmount=${loan.nextPaymentAmount.toString()}`,
+    )
+    const tx: ethers.ContractTransactionResponse = await this.poolContract.refundLoan(nftId, borrowerAddress, {
+      value: loan.nextPaymentAmount.toString(),
+    })
+
+    const receipt = await tx.wait()
+    if (receipt?.status !== 1) {
+      console.error('[nemeos-sdk][payNextLoanStep] Transaction failed! Tx receipt', receipt)
+      throw new NemeosSDKError('Pool.refundLoan() Transaction failed!')
+    }
+    console.log('[nemeos-sdk][payNextLoanStep] Transaction successful! Tx receipt', receipt)
     return receipt
   }
 
@@ -100,3 +136,46 @@ class NemeosPool {
       : { maxFeePerGas: feeData.maxFeePerGas, maxPriorityFeePerGas: feeData.maxPriorityFeePerGas }
   }
 }
+
+export type Loan = {
+  /** Borrower address */
+  borrower: string
+  /** Token ID */
+  tokenID: bigint
+  /** Amount owed with interest */
+  amountOwedWithInterest: bigint
+  /** Next payment amount */
+  nextPaymentAmount: bigint
+  /** Interest amount per payment */
+  interestAmountPerPayment: bigint
+  /** Loan duration in seconds */
+  loanDurationInSeconds: bigint
+  /** Start time */
+  startTime: bigint
+  /** Next payment time */
+  nextPaymentTime: bigint
+  /** Remaining number of installments */
+  remainingNumberOfInstallments: number
+  /** Daily interest rate at start */
+  dailyInterestRateAtStart: bigint
+  /** Is closed */
+  isClosed: boolean
+  /** Is in liquidation */
+  isInLiquidation: boolean
+}
+
+/** The response object returned by ethers.js is an array wrapped by a Proxy, unwrap the proxy for easier external usage */
+const loanProxyToObjectMapper = (loan: Loan): Loan => ({
+  borrower: loan.borrower,
+  tokenID: BigInt(loan.tokenID),
+  amountOwedWithInterest: BigInt(loan.amountOwedWithInterest),
+  nextPaymentAmount: BigInt(loan.nextPaymentAmount),
+  interestAmountPerPayment: BigInt(loan.interestAmountPerPayment),
+  loanDurationInSeconds: BigInt(loan.loanDurationInSeconds),
+  startTime: BigInt(loan.startTime),
+  nextPaymentTime: BigInt(loan.nextPaymentTime),
+  remainingNumberOfInstallments: Number(loan.remainingNumberOfInstallments),
+  dailyInterestRateAtStart: BigInt(loan.dailyInterestRateAtStart),
+  isClosed: loan.isClosed,
+  isInLiquidation: loan.isInLiquidation,
+})
